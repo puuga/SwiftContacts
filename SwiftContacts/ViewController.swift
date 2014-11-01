0629e061012d0861ed84a0bfbc321ade3f0515e5
puuga <siwaoh@gmail.com>
2014-11-01 13:45:02 +0700

    Initial Commit

---
+++ SwiftContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftContacts
+//
+//  Created by Siwawes Wongcharoen on 11/1/2557 BE.
+//  Copyright (c) 2557 puuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
